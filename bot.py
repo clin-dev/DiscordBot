4b4df6d4317ab0a9e65c8a6e81459b6f5fef972b
clin-dev <chaolinle@gmail.com>
2021-01-03 20:54:25 -0500

    added on member join event handler

--- bot.py
+++ bot.py
@@ -4,12 +4,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-print(TOKEN)
+GUILD = os.getenv('DISCORD_GUILD')
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+
+"""
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+"""
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hello {member.name}, welcome!'
+    )
 
 client.run(TOKEN)
 
